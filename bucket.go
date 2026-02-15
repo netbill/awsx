3575700afa01514c02558c04d25cd90739ef1061
paranoideed <yehorushakov@gmail.com>
2026-02-15 16:35:22 +0200

    update and wrapp error

--- bucket.go
+++ bucket.go
@@ -11,35 +11,29 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
-type Bucket struct {
+type Bucket interface {
+	PresignPut(ctx context.Context, key string, ttl time.Duration) (uploadURL, getURL string, err error)
+	HeadObject(ctx context.Context, key string) (*s3.HeadObjectOutput, error)
+	GetObject(ctx context.Context, key string) (io.ReadCloser, error)
+	GetObjectRange(ctx context.Context, key string, bytes int64) (io.ReadCloser, error)
+	CopyObject(ctx context.Context, fromKey, toKey string) error
+	DeleteObject(ctx context.Context, key string) error
+}
+
+type bucket struct {
 	name    string
 	client  *s3.Client
 	presign *s3.PresignClient
 }
 
-func New(
-	name string,
-	client *s3.Client,
-	presign *s3.PresignClient,
-) *Bucket {
-	return &Bucket{
-		name:    name,
-		client:  client,
-		presign: presign,
-	}
+func New(name string, client *s3.Client, presign *s3.PresignClient) Bucket {
+	return &bucket{name: name, client: client, presign: presign}
 }
 
-func (b *Bucket) PresignPut(
-	ctx context.Context,
-	key string,
-	ttl time.Duration,
-) (uploadURL, getURL string, err error) {
+func (b *bucket) PresignPut(ctx context.Context, key string, ttl time.Duration) (uploadURL, getURL string, err error) {
 	putOut, err := b.presign.PresignPutObject(
 		ctx,
-		&s3.PutObjectInput{
-			Bucket: aws.String(b.name),
-			Key:    aws.String(key),
-		},
+		&s3.PutObjectInput{Bucket: aws.String(b.name), Key: aws.String(key)},
 		s3.WithPresignExpires(ttl),
 	)
 	if err != nil {
@@ -48,10 +42,7 @@ func (b *Bucket) PresignPut(
 
 	getOut, err := b.presign.PresignGetObject(
 		ctx,
-		&s3.GetObjectInput{
-			Bucket: aws.String(b.name),
-			Key:    aws.String(key),
-		},
+		&s3.GetObjectInput{Bucket: aws.String(b.name), Key: aws.String(key)},
 		s3.WithPresignExpires(ttl),
 	)
 	if err != nil {
@@ -61,84 +52,64 @@ func (b *Bucket) PresignPut(
 	return putOut.URL, getOut.URL, nil
 }
 
-func (b *Bucket) HeadObject(
-	ctx context.Context,
-	key string,
-) (*s3.HeadObjectOutput, error) {
-	output, err := b.client.HeadObject(ctx, &s3.HeadObjectInput{
+func (b *bucket) HeadObject(ctx context.Context, key string) (*s3.HeadObjectOutput, error) {
+	res, err := b.client.HeadObject(ctx, &s3.HeadObjectInput{
 		Bucket: aws.String(b.name),
 		Key:    aws.String(key),
 	})
-
-	return output, err
+	if err != nil {
+		return nil, Wrap(err)
+	}
+	return res, nil
 }
 
-func (b *Bucket) GetObject(
-	ctx context.Context,
-	key string,
-) (body io.ReadCloser, err error) {
+func (b *bucket) GetObject(ctx context.Context, key string) (io.ReadCloser, error) {
 	out, err := b.client.GetObject(ctx, &s3.GetObjectInput{
 		Bucket: aws.String(b.name),
 		Key:    aws.String(key),
 	})
 	if err != nil {
-		return nil, err
+		return nil, Wrap(err)
 	}
 	if out.Body == nil {
-		return nil, fmt.Errorf("s3 get object: body is nil")
+		return nil, fmt.Errorf("awsx: get object: body is nil")
 	}
-
 	return out.Body, nil
 }
 
-func (b *Bucket) GetObjectRange(
-	ctx context.Context,
-	key string,
-	bytes int64,
-) (io.ReadCloser, error) {
+func (b *bucket) GetObjectRange(ctx context.Context, key string, bytes int64) (io.ReadCloser, error) {
 	if bytes <= 0 {
 		return b.GetObject(ctx, key)
 	}
 
 	rng := "bytes=0-" + strconv.FormatInt(bytes-1, 10)
-
 	out, err := b.client.GetObject(ctx, &s3.GetObjectInput{
 		Bucket: aws.String(b.name),
 		Key:    aws.String(key),
 		Range:  aws.String(rng),
 	})
 	if err != nil {
-		return nil, err
+		return nil, Wrap(err)
 	}
 	if out.Body == nil {
-		return nil, fmt.Errorf("s3 get object range: body is nil")
+		return nil, fmt.Errorf("awsx: get object range: body is nil")
 	}
 	return out.Body, nil
 }
 
-func (b *Bucket) CopyObject(
-	ctx context.Context,
-	fromKey, toKey string,
-) (string, error) {
+func (b *bucket) CopyObject(ctx context.Context, fromKey, toKey string) error {
 	_, err := b.client.CopyObject(ctx, &s3.CopyObjectInput{
 		Bucket:     aws.String(b.name),
 		Key:        aws.String(toKey),
 		CopySource: aws.String(b.name + "/" + fromKey),
 	})
-	if err != nil {
-		return "", err
-	}
-
-	return toKey, nil
+	return Wrap(err)
 }
 
-func (b *Bucket) DeleteObject(
-	ctx context.Context,
-	key string,
-) error {
+func (b *bucket) DeleteObject(ctx context.Context, key string) error {
 	_, err := b.client.DeleteObject(ctx, &s3.DeleteObjectInput{
 		Bucket: aws.String(b.name),
 		Key:    aws.String(key),
 	})
-	return err
+	return Wrap(err)
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.24.4
 require (
 	github.com/aws/aws-sdk-go-v2 v1.41.1
 	github.com/aws/aws-sdk-go-v2/service/s3 v1.95.1
+	github.com/aws/smithy-go v1.24.0
+	github.com/netbill/ape v0.1.3
 )
 
 require (
@@ -16,5 +18,4 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.9.8 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.13.17 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.19.17 // indirect
-	github.com/aws/smithy-go v1.24.0 // indirect
 )
